d0bb7b45498540161f68ac6dc0201e14e14976b1
elizaNev <114655512+elizaNev@users.noreply.github.com>
2022-10-06 23:31:27 +0400

    second version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,28 +4,23 @@ var rows: Int = 6
 var columns: Int = 7
 var enterColumns = 0
 var enterRows = 0
-fun readPlayers():String {
+fun readPlayers() {
     println("First player's name:")
     firstPlayer = readln()
     println("Second player's name:")
     secondPlayer = readln()
-    return firstPlayer
-    return secondPlayer
 }
-fun splitIntoRowsAndColumns(sizeBoard: String): Int {
-    var splitBoard = sizeBoard.lowercase().split("x")
+fun splitIntoRowsAndColumns(sizeBoard: String) {
+    var splitBoard = sizeBoard.split("x")
     enterRows = splitBoard.first().trim().toInt()
     enterColumns = splitBoard.last().trim().toInt()
-    return enterRows
-    return enterColumns
 }
 fun createBoard() {
     println("Set the board dimensions (Rows x Columns)")
     println("Press Enter for default (6 x 7)")
-    val sizeBoard = readln()
-    val formatWithSpace = Regex(".?. x?X? .?.")
-    val formatWithoutSpace = Regex(".?.x?X?.?.")
-    if (formatWithSpace.matches(sizeBoard) || formatWithoutSpace.matches(sizeBoard)) {
+    val sizeBoard = readln().lowercase()
+    var format = Regex(".?. ?x ?.?.")
+    if (format.matches(sizeBoard)) {
         splitIntoRowsAndColumns(sizeBoard)
         if ((enterRows > 4 && enterRows < 10) && (enterColumns > 4 && enterColumns < 10)) {
             println("$firstPlayer VS $secondPlayer")
@@ -44,7 +39,7 @@ fun createBoard() {
         }
     }
 }
-fun main(args: Array<String>) {
+fun main() {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Connect Four")
